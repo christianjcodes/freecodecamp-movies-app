2b8711fa2dbcb97714cbfa5387c3b30f06d37e31
Christian Johnson <cartian4@gmail.com>
2023-10-03 06:04:20 -0400

    Add ResponseEntity

--- src/main/java/com/christianjcodes/movies/MovieController.java
+++ src/main/java/com/christianjcodes/movies/MovieController.java
@@ -1,5 +1,7 @@
 package com.christianjcodes.movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
     @GetMapping
-    public String allMovies() {
-        return "All Movies!";
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
     }
 
 }
