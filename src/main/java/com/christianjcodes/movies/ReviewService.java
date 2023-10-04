74886462b2e63d8b0aea895ecda54e3958cf7ce3
Christian Johnson <cartian4@gmail.com>
2023-10-04 05:37:33 -0400

    Create ReviewService and add createReview method, add constructor to Review.java

---
+++ src/main/java/com/christianjcodes/movies/ReviewService.java
@@ -0,0 +1,13 @@
+package com.christianjcodes.movies;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = new Review(reviewBody);
+
+
+    }
+
+}
